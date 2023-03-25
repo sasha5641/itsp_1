171356610765b19271e7329119d29e8da52072ad
sasha5641 <ostapenko200019@gmail.com>
2023-03-25 14:21:11 +0200

    classwork

---
+++ lesson7.4.py
@@ -0,0 +1,17 @@
+def checker(function, *args, **kwargs):
+    def checker(*args, **kwargs):
+        try:
+            result = function(*args, **kwargs)
+        except Exception as exc:
+            print(f'We have a problem: {exc}')
+        else:
+            print(f'No problems, result: {result}')
+    return checker
+
+@checker     #Декоратор
+def calculate(expression):
+    return eval(expression)
+
+
+calculate('2+2')
+
