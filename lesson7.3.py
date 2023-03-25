1201a7684c0d1a4d11b8655ef5dc12036b8589c7
sasha5641 <ostapenko200019@gmail.com>
2023-03-25 14:03:50 +0200

    classwork

---
+++ lesson7.3.py
@@ -0,0 +1,29 @@
+class Counter:
+    def __init__(self, max_number):
+        self.i = 0
+        self.max_number = max_number
+
+    def __iter__(self):
+        self.i = 0
+        return self
+
+    def __next__(self):
+        self.i += 1
+        if self.i > self.max_number:
+            raise StopIteration
+        return self.i
+
+
+count = Counter(25)
+
+#for element in count:
+   # print(element)
+
+
+
+   #те ж саме
+
+
+
+#iterator = iter(count)
+#print(iterator)
