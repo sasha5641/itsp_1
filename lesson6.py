b1b1b07599390d34d17d354d4c4660dfd9450d2f
sasha5641 <ostapenko200019@gmail.com>
2023-03-25 12:54:01 +0200

    classwork

--- lesson6.py
+++ lesson6.py
@@ -1,14 +1,34 @@
+class MyExcepsion(Exception):
+    def __str__(self):
+        return 'Hello! It`s my exception!'
+
+
 #Обробник виключень
+    #def sum_numbers(num1, num2, num3):
+        #try:
+            #num1 = int(num1)  #int ctili chisla
+            #num2 = int(num2)            #можна флоат
+           # num3 = int(num3)
+
+           # res = num1 / num2 / num3
+           # print(res)
+        #except ValueError:   #Спрацьовує, якщо блок try вибиває помилку + rkscho  валює ерор воно на 1 помилку валює ерор
+            #print('Ви ввели не число!')
+        #except ZeroDivisionError:   #Спрацьовує, якщо блок try вибиває помилку + на нуль ділити не можна (ошибка 1 )
+           # print('Ділити на 0 не можна!')
+       # except Exception:    #Обичний ексцепт на все работает + exception toge takoe ge
+           # print('Pomilka')
+       # except Exception as exc:
+            #print(exc)
+        #else:
+           # print('Виконано без помилок!')
+        #finally:
+            #print('Кінець програми!')
 def sum_numbers(num1, num2, num3):
-    num1 = int(num1)  #int ctili chisla
-    num2 = int(num2)
-    num3 = int(num3)
-
-    res = num1 + num2 + num3
-    return res
-
+    if not num1.isdigit() or not num2.isdigit() or not num3.isdigit():
+        raise MyExcepsion #Викликає спеціально помилку
 
 n_1 = input('Введіть число 1: ')
 n_2 = input('Введіть число 2: ')
 n_3 = input('Введіть число 3: ')
-print(sum_numbers(n_1, n_2, n_3))
+sum_numbers(n_1, n_2, n_3)
